5a2a446e2d43eb54362cd85521af5d10eaf17839
Andreas <andreasrajagoklassitorus@gmail.com>
2024-04-05 23:49:50 +0200

    simu-NIF-1DOF not working properly.

--- NIF/Objective_Function.m
+++ NIF/Objective_Function.m
@@ -1,48 +1,70 @@
-function Obj_Val = Objective_Function(Estimation_Var, opt_weights, scales, dt, stop_time, accFunargs)
+function Obj_Val = Objective_Function(Est_Var, opt_mode, Est_Scales, simuArgs)
 global Param
 %% Get the measured data
 % Velo mea should span over all the time steps.
 % Thus extract the data directly
-temp_Velo_Mea = squeeze(Param.NIF_nu_b).';
-% Velo_Mea = temp_Velo_Mea(:, data);
-Velo_Mea = temp_Velo_Mea;
+temp_Velo_Mea = squeeze(Param.NIF_nu_b);
+Velo_Mea = temp_Velo_Mea(:, opt_mode);
 
 %% Get the estimation data
 % Scales back Estimation_Var_Scaled to Estimation_Var
-Estimation_Var = Estimation_Var .* scales;
+Est_Var = Est_Var .* Est_Scales;
 
-% Get the optimization variables
-Param.NIF_AM = Estimation_Var(1:6);
-Param.NIF_K_l = Estimation_Var(7:12);
-Param.NIF_K_nl = Estimation_Var(13:18);
-Param.NIF_Ballast_Force = [0 ; 0; Estimation_Var(19:21)'; 0];
+%% Run Simulink Model
+% Extract Simulink arguments
+simuModelName = simuArgs{1};
+simuModelFileName = simuArgs{2};
+dt = simuArgs{3};
+stop_time = simuArgs{4};
 
-% Get the estimated velocity
-[~, nu_b] = BlueROV2_Dynamic_Model(dt, stop_time, accFunargs);
-% Velo_Est = nu_b(:, data);
-Velo_Est = nu_b;
+% Build the simulink parameter block
+NIF_AM = zeros(1,6);
+NIF_K_l = zeros(1,6);
+NIF_K_nl = zeros(1,6);
+NIF_Ballast_Force = zeros(6,1);
+
+% Load estimation variable to the simulink block parameter
+NIF_AM(1, opt_mode) = Est_Var(1);
+NIF_K_l(1, opt_mode) = Est_Var(2);
+NIF_K_nl(1, opt_mode) = Est_Var(3);
+NIF_Ballast_Force(opt_mode, 1) = Est_Var(4);
+
+% Open System NIF Simulink
+open_system(simuModelFileName);
+
+% Set time
+set_param(simuModelName, 'StopTime', num2str(stop_time));
+
+% Set block value inside the model
+set_param([simuModelName '/Added_Mass'], 'Value', mat2str(NIF_AM))
+set_param([simuModelName '/Linear_Damping'], 'Value', mat2str(NIF_K_l))
+set_param([simuModelName '/Nonlinear_Damping'], 'Value', mat2str(NIF_K_nl))
+set_param([simuModelName '/Ballast_Force'], 'Value', mat2str(NIF_Ballast_Force))
+
+% Full path to the "Ballast" block within the subsystems
+ballastBlockPath = [simuModelName '/For Estimation/BlueROV2/Ballast Force in Body Frame/Ballast'];
+
+% Setting the 'Before' and 'After' parameters of the "Ballast" Step block
+set_param(ballastBlockPath, 'Before', mat2str(NIF_Ballast_Force));
+set_param(ballastBlockPath, 'After', mat2str(NIF_Ballast_Force));
+
+% Run the Simulink model
+simOutNIF = sim(simuModelName, 'ReturnWorkspaceOutputs', 'on');
+
+% Get the Velocity
+temp_Velo_Est = squeeze(simOutNIF.Est_Velo_B_S)';
+Velo_Est = temp_Velo_Est(:, opt_mode);
 
 %% Convergence coefficient
 G_delta = 100;
 
 %% Estimation-Measurement Error
-delta = Velo_Est - Velo_Mea;
+Delta = Velo_Est - Velo_Mea;
 
 %% Compute cost value
-% Get the velocity matrix size
-[~, column_length] = size(delta);
-
-% Comput the nominator and denuminator
-nominator = zeros(1, column_length);
-denuminator = zeros(1, column_length);
-for column = 1 : column_length
-    nominator(1, column) = delta(:, column)' * delta(:, column);
-    denuminator(1, column) = Velo_Mea(:, column)' * Velo_Mea(:, column);
-end
-
-% Sum-weighted nominator
-sum_weight_nominator = opt_weights * nominator';
-sum_denuminator = sum(denuminator);
+% Compute the sum-weighted nominator
+sum_weight_nominator = Delta' * Delta;
+sum_denuminator = Velo_Mea' * Velo_Mea;
 
 % Compute the objective value
 Obj_Val = (sum_weight_nominator / sum_denuminator) * G_delta;
